5734e10b86c23f4a442a6704e0e4fc6e05a666bd
Ramin Anvar <ramina1964@gmail.com>
2020-12-22 00:31:03 +0100

    Small changes to GUI

--- NQueen.GUI/ViewModel/MainViewModel.cs
+++ NQueen.GUI/ViewModel/MainViewModel.cs
@@ -6,8 +6,7 @@ namespace NQueen.GUI.ViewModel
     public class MainViewModel : ViewModelBase
     {
         public MainViewModel(ISolver solver) => SolverViewModel = new SolverViewModel(solver);
-        #region PublicProperties
+
         public SolverViewModel SolverViewModel { get; }
-        #endregion PublicProperties
     }
 }
